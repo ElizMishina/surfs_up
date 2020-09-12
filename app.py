99e5233e0870e8971824eb9f0e52ef4f0fabdb5f
EMish <Liza.s.mishina@gmail.com>
2020-09-12 13:24:23 -0700

    9.4.3

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+import app
+
+
+
+print("example __name__ = %s", __name__)
+
+if __name__ == "__main__":
+    print("example is being run directly.")
+else:
+    print("example is being imported")
